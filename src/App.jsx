c2baf0c44dcc5310aaf74c3698be8e098b338250
mukeshthakur0 <mitu4846t@gmail.com>
2026-06-26 10:27:34 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Education from "./components/education";
+import Experience from "./components/experience";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
 
 function App() {
   return (
